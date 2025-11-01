6a1b21815886e003a83f030888d72c9a569b7a5f
DavidoSF <david-sfeir@hotmail.com>
2025-11-01 17:13:53 +0100

    todo

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -7,10 +7,12 @@ router = APIRouter(prefix="/admin", tags=["admin"])
 
 admin_activities = []
 
+
 @router.post("/activity", response_model=Activity)
 def add_activity(activity: Activity):
     admin_activities.append(activity)
     return activity
+
 
 @router.get("/activities", response_model=List[Activity])
 def list_admin_activities():
